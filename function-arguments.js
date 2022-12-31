fb8d2df043e6e4af8a799749a8ee8f3d33182817
Ramy Mohamed <ramy.opene@gmail.com>
2022-12-31 05:25:58 -0500

    Initial commit with all my work so far

---
+++ function-arguments.js
@@ -0,0 +1,4 @@
+let math = function(number1, number2, number3){
+    return number2*number3+number1
+}
+console.log(math(53,61,67))
---
+++ looping-through-arrays.js
@@ -0,0 +1,6 @@
+let pets = ['cat', 'dog', 'rat']
+
+for(let i = 0; i <= 2; i++){
+    pets[i] = pets[i] + 's'
+}
+console.log(pets)
